d23ef58b2c12fd9e745c7116e57c57d277ab80c3
caramelito37 <davidhuallpaq@gmail.com>
2024-07-09 02:37:44 -0500

    front  del chat

--- app/Http/Controllers/ChatGemmaController.php
+++ app/Http/Controllers/ChatGemmaController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 
 class ChatGemmaController extends Controller
 {
     public function index()
     {
-        return Inertia::render('gemma/index');
+        return Inertia::render('gemma/index', [
+            'user' => Auth::user()->name,
+        ]);
     }
 }
